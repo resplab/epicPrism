86d82b46b081cc45f65b6d05cfd0b73f8874c16c
Adibi <maadibi@ead.ubc.ca>
2019-02-20 16:55:31 -0800

    making epicPrism minimal

--- R/package-init.R
+++ R/package-init.R
@@ -1,20 +1,4 @@
-#' @useDynLib epicR, .registration=TRUE
-#' @importFrom Rcpp sourceCpp
-#' @importFrom Rcpp evalCpp
-#' @importFrom graphics barplot legend lines pie plot title
-#' @importFrom stats aggregate binomial coefficients glm optim runif confint
-#' @importFrom utils packageVersion
-#' @importFrom utils write.csv
-#' @import ggplot2
-#' @import ggthemes
-#' @import dplyr
-#' @importFrom scales pretty_breaks
-#' @importFrom survival survfit
-#' @importFrom survminer ggsurvplot
 #' @import jsonlite
+#' @import epicR
 NULL
-
-.onUnload <- function (libpath) {
-  library.dynam.unload("epicR", libpath)
-}
 
